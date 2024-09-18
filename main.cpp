3bef8a2d95850a5492854161d4ab749bb6f6c079
David Wangai <david.wangai@strathmore.edu>
2024-09-18 10:52:31 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,40 @@
+//Code contributed by David Wangai
+#include <algorithm>
+#include <iostream>
+#include<fstream>
+#include<vector>
+using namespace std;
+int main()
+{
+   struct fixtures;
+    {
+        string teams;
+        string stadium;
+        string town;
+        string day;
+
+    }
+    //reading csv file(geeksforgeeks)
+    void create();
+    {
+        //Opening an existing csv file
+        "teams_csvFile.csv";
+        cout<<"Enter the team details:";
+    }
+    vector<string> Teams = {get.file_from_teams_csvFile.csv};
+    generate.fixtures;
+    {
+        ;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
