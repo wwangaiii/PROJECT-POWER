00d806334f6ef7258e7b618e08453b5c02fe1b5f
freebaraka <baraka.onserio@strathmore.edu>
2024-10-11 11:54:46 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -40,20 +40,19 @@ void loadTeams(const string& filename, vector<Team>& teams) {
     }
 
 void generateFixtures(const vector<Team>& teams, vector<Match>& fixtures) {
-    int totalTeams = teams.size();
+    int totalTeams = teams.size();// Stores the total number of teams
     int totalWeekends = (totalTeams - 1) * 2; // Each team plays every other team twice
 //Create fixtures for each team
-for(int leg=1;i<=2;leg++)
-{for(int j=i+1;j<totalTeams;j++)
-{Match match;
-match.home=teams[i];
-match.away=teams[j];
-match.leg=leg;
-match.time=(rand()%2==0)?
-    "afternoon":"evening";
+for(int leg=1;i<=2;leg++){// Two legs for each team(HOME AND AWAY)
+   for (int i = 0; i < totalTeams; i++) {//Iterates through each team as a potential home team. 
+     for(int j=i+1;j<totalTeams;j++){// Iterates through each team after the current home team, ensuring that each pair of teams is matched only once per leg.
+Match match;//creating the object(Match)
+match.home=teams[i];//setting the home teams as "i"
+match.away=teams[j];//setting away teams as "j"
+match.leg=leg;//assigning current leg(EITHER 1 OR 2)
+match.time=(rand()%2==0) ? "afternoon":"evening";;//randomly assigning the time
 match.weekend="weekend="+to_string((i+j+leg)%totalWeekends+1);
-match.day=(rand()%2==)?
-    "Saturday":"Sunday";
+match.day=(rand()%2==) ? "Saturday":"Sunday";//randomly assigning the day
 fixtures.push_back(match);
 }
 }
@@ -70,7 +69,7 @@ fixtures.push_back(match);
              <<match.day << ", "
              << match.weekend << "\n";
 
-        cout << match.home.name << " VS " << match.away.name
+        cout << match.home.name << " VS " << match.away.name//output formula
              << "  |  Leg: " << match.leg
              << "  |  Time: " << match.time
              << "  |  " << match.weekend
